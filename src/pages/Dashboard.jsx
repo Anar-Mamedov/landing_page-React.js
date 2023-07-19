28de905766ae8d0315bea0b80a11cae03c2a058c
Anar Mamedov <anar26271@gmail.com>
2023-07-20 00:12:01 +0300

    Skills section

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
 import About from "../components/About/About";
+import Skills from "../components/Skills/Skills";
 
 const Dashboard = () => {
   return (
@@ -9,6 +10,7 @@ const Dashboard = () => {
       <Header />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 };
